55795cb56d8d0399d9025c75dc6ffe0117621dee
Pscabby <pscabby0506@gmail.com>
2022-12-06 22:46:01 +0800

    Initial Commit

---
+++ disney_plus/disney_plusApp.swift
@@ -0,0 +1,17 @@
+//
+//  disney_plusApp.swift
+//  disney_plus
+//
+//  Created by Abby on 2022/12/6.
+//
+
+import SwiftUI
+
+@main
+struct disney_plusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
